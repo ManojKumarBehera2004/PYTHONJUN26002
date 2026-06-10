9fededf09642abe976f2d4419a4f443a231f6083
ManojKumarBehera2004 <trishultrishul619@gmail.com>
2026-06-10 17:53:39 +0530

    assignment file

---
+++ Month-1/Week-1/Day-2/index.py
@@ -0,0 +1,34 @@
+# school = "ABC School"
+# age = 23
+# address = '123 main strret'
+# building_name = "Sky Tower"
+
+# Data Type
+
+# Preemptive Data Type
+# name = "Suraj"
+# age = 23
+# cgpa = 9.5
+# is_valid = True
+
+# print(type(age))
+
+#name = input("Enter your full name : ")
+# age = input("Enter your age : ")
+# age_in_number = str(age)
+# print("your age is : ", int (age))
+# x = int(input("Enter the value of x : "))
+# y = int(input("Enter the value of y: "))
+ #addition
+# print("The sum of x and y is : ", x+y)
+
+#Substraction
+#print("The sum of x and y is : ", x-y)
+# print (True and False)
+# print(True or False)
+# print(not True)
+
+#COMPARISION OPERATER
+
+age = input("Enter your age  ; ")
+print(age == 5)
